854738ff943ed443e5261c67044d7f4d64fe3846
Alexandr Dobrovolskiy <sasha732sasha000@gmail.com>
2017-10-11 13:21:27 +0300

    trapezy solution

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using static System.Console;
+using static System.Double; //  We'll not use enother types in this solution, it's bed example for other problems;
+using static System.Math;
 
 namespace Task3
 {
@@ -6,7 +9,15 @@ namespace Task3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WriteLine("Enter the basises of trapeze and the angle on largest basis ( separated by lines ) :");
+
+            double firstBasis = Parse(ReadLine());
+            double secondBasis = Parse(ReadLine());
+            double angle = Parse(ReadLine());
+
+            WriteLine($"The result is { ((firstBasis + secondBasis) / 2) *  Abs( (Abs(firstBasis - secondBasis) / 2 ) * Tan(angle) ) }");
+
+            ReadKey();
         }
     }
 }
