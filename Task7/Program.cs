60ec90640d5b65483d210124274fc743e46360ed
Alexandr Dobrovolskiy <sasha732sasha000@gmail.com>
2017-10-11 18:39:46 +0300

    task 7

---
+++ Task7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("Enter your number :");
+            ulong num = ulong.Parse(ReadLine());
+            ulong sum = 0;
+
+            do
+            {
+                sum += num % 10;
+                num /= 10;
+
+            } while (num != 0);
+
+            WriteLine($"The result is {sum}.");
+
+            ReadKey();
+        }
+    }
+}
