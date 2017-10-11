aa51096f830a4bdcacb80e20db1d1f845d06ff13
Alexandr Dobrovolskiy <sasha732sasha000@gmail.com>
2017-10-11 17:44:34 +0300

    task 5

---
+++ Task5/Equation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson1
+{
+    class Equation
+    {
+        private double a, b, c, desc;
+
+        public Equation(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            this.desc = this.GetDesc();
+        }
+
+        public double GetDesc()
+        {
+            return Math.Pow(b, 2) - (4 * a * c);
+        }
+
+        public String toString()
+        {
+            return $"I'm equation, \n {a}x^2 + {b}x + {c} = 0 ";
+        }
+
+        public string getRoots()
+        {
+            if (desc > 0)
+                return $"x1 = {(-1 * b + Math.Sqrt(desc)) / 2 * a}, x2 = {(-1 * b - Math.Sqrt(desc)) / 2 * a}.";
+            else if (desc == 0)
+                return $"x1 = {(-1 * b) / 2 * a}, x2 = not a root.";
+            else
+                return "there is no roots ";
+        }
+    }
+}
---
+++ Task5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using static System.Double;
+using static System.Console;
+
+namespace Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Enter A, B and C :");
+
+            double A = Parse(ReadLine());
+            double B = Parse(ReadLine());
+            double C = Parse(ReadLine());
+
+            Equation myEquation = new Equation(A, B, C);
+
+            WriteLine(myEquation.toString());
+            WriteLine(myEquation.getRoots());
+            ReadKey();
+        }
+    }
+}
+
