4ecb8adc2f86853665a5a5c1ad0af09682a35711
AlexandrDobrovolskiy <sasha732sasha000@gmail.com>
2017-10-18 00:46:52 +0300

    task 12 daemn

---
+++ Task12/Point.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task12
+{
+    class Point
+    {
+        private double x, y;
+
+        public double getX() { return x; }
+        public double getY() { return y; }
+        
+        public Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public double getDistance(Point enotherPoint)
+        {
+            return Math.Sqrt( Math.Pow(this.getX() - enotherPoint.getX(), 2) + Math.Pow(this.getY() - enotherPoint.getY(), 2));
+        }
+    }
+}
---
+++ Task12/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point a = new Point(0,1);
+            Point b = new Point(0, -1);
+            Point c = new Point(1, 0);
+
+            Triangle myTriangle = new Triangle(a,b,c);
+            Console.WriteLine(myTriangle.getPerimeter());
+        }
+    }
+}
