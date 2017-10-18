7b416035e73a7de6029a3b0e29e8ad40b408bbb0
AlexandrDobrovolskiy <sasha732sasha000@gmail.com>
2017-10-18 12:58:30 +0300

    task 12 finaly

--- Task12/Program.cs
+++ Task12/Program.cs
@@ -6,12 +6,33 @@ namespace Task12
     {
         static void Main(string[] args)
         {
-            Point a = new Point(0,1);
-            Point b = new Point(0, -1);
-            Point c = new Point(1, 0);
+            Console.WriteLine("Enter 10 points please: ");
+            
+            Point[] points = new Point[10];
+            String[] input;
 
-            Triangle myTriangle = new Triangle(a,b,c);
-            Console.WriteLine(myTriangle.getPerimeter());
+            for (int i = 0; i < 10; i++)
+            {
+                input = Console.ReadLine().Split();
+                points.SetValue(new Point(int.Parse(input[0]), int.Parse(input[1])), i);
+            }
+
+
+            double[] distanses = new double[100];
+
+            for(int i = 0; i < 10; i++)
+            {
+                for (int j = 1; j < 10; j++)
+                {
+                    distanses.SetValue(points[i].getDistance(points[j]),(i * 10) + j);
+                }
+            }
+
+            Array.Sort(distanses);
+
+            Console.WriteLine($"The biggest perimeter is {distanses[99] + distanses[98] + distanses[97]}");
+
+            Console.ReadKey();
         }
     }
 }
