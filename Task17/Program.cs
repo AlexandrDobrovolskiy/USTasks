88e9912031af2f957d4e33e513cd59174682d659
AlexandrDobrovolskiy <sasha732sasha000@gmail.com>
2017-10-26 23:43:05 +0300

    task 17 comments

--- Task17/Program.cs
+++ Task17/Program.cs
@@ -10,8 +10,11 @@ namespace Task17
         static void Main(string[] args)
         {
             WriteLine("Enter points you want (example 0,0 1,2 3,4...) : ");
-            String[] inputs = ReadLine().Split();
 
+            // Парсинг инпута
+            String[] inputs = ReadLine().Split(); 
+
+            //Создание списка который хранит заданые точки
             List<Point> points = new List<Point>(); 
 
             foreach (String point in inputs)
@@ -20,15 +23,18 @@ namespace Task17
                 points.Add(new Point(Parse(coords[0]), Parse(coords[1])) );
             }
 
-            List<Section> sections = new List<Section>();
+            //Создание списка который хранит грани многоугольника
+            List<Section> sections = new List<Section>();   
 
             for (int i = 0; i < points.Count - 1 ; i++)
                 sections.Add(new Section(points[i], points[i + 1]));
 
-            Section last = sections[sections.Count - 1];
+            Section last = sections[sections.Count - 1];    
 
+            //Проверяем не лежит ли последняя точка на первой грани и не пересекаются ли первая и последняя грань
             bool isPoligon = !(Section.Rotate(sections[0].A, sections[0].B, last.B) == 0);
 
+            //Ищем пересекающиеся грани
             for (int i = 1; i < sections.Count; i++)
             {
                 if (!isPoligon)
