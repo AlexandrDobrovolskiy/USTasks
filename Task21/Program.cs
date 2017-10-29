2c90ae607e7a27485034bd67a2a01050ac0ff4e9
AlexandrDobrovolskiy <sasha732sasha000@gmail.com>
2017-10-30 00:21:43 +0200

    task 21

--- Task21/Program.cs
+++ Task21/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using static System.Console;
 
 namespace Task21
 {
     class Program
     {
+
+        static void Inverses(int[] array, ref int count)
+        {
+            count = 0;
+
+            for (int i = 0; i < array.Length - 1; i++)
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                        count++;
+                }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WriteLine("Enter array (separate values by space): ");
+
+            string[] inputs = ReadLine().Split();
+            int[] arr =  new int[inputs.Length];
+
+            for(int i = 0; i < inputs.Length; i++)
+                arr.SetValue(int.Parse(inputs[i]), i);
+
+            int count = 0;
+
+            Inverses(arr, ref  count);
+
+            WriteLine($"The count of inverses is {count}");
+
+            ReadKey();
         }
     }
 }
