e96dbef2ba96edc14ac2f2903ffb984c0d9beb2c
AlexandrDobrovolskiy <sasha732sasha000@gmail.com>
2017-10-30 01:19:54 +0200

    1-3

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
+using static System.Console;
+
 
 namespace Task27
 {
     class Program
     {
+
+        public static string GetDecomposition(int n)
+        {
+            string res = "";
+
+            for (int i = 2; true;)
+            {
+                if (n % i != 0)
+                {
+                    i++;
+                    continue;
+                }
+                int k = n / i;
+                res += $"{i} ";
+                n = k;
+
+                if (n == 1)
+                    break;
+
+                res += "x ";
+            }
+
+            return res;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Write("Enter n: ");
+            int n = int.Parse(ReadLine());
+
+            WriteLine($"The result is {GetDecomposition(n)}");
+            ReadKey();
         }
     }
 }
---
+++ Task3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static System.Console;
+using static System.Double; //  We'll not use enother types in this solution, it's bed example for other problems;
+using static System.Math;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Enter the basises of trapeze and the angle on largest basis ( separated by lines ) :");
+
+            double firstBasis = Parse(ReadLine());
+            double secondBasis = Parse(ReadLine());
+            double angle = Parse(ReadLine());
+
+            WriteLine($"The result is { ((firstBasis + secondBasis) / 2) *  Abs( (Abs(firstBasis - secondBasis) / 2 ) * Tan(angle) ) }"); 
+
+            ReadKey();
+        }
+    }
+}
