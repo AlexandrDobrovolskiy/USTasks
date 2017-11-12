43ba2c2339ce2b07da123f91e0c5d283c1fb2221
AlexandrDobrovolskiy <sasha732sasha000@gmail.com>
2017-11-12 22:41:56 +0200

    frfsd

--- Task31/Algorithms.cs
+++ Task31/Algorithms.cs
@@ -8,21 +8,20 @@ namespace Task31
         public static void SplitNum(int[] arrN)
         {
             int lastNum = arrN.Last();
+
             for (int i = 0; i < lastNum / 2; i++)
             {
                 for (int j = 0; j < arrN.Length - 1; j++)
-                {
                     Console.Write(arrN[j] + "+");
-                }
+
                 Console.WriteLine($"{(lastNum / 2) - i}+{lastNum - ((lastNum / 2) - i)}");
             }
             if (lastNum != 1)
             {
                 int[] newArrN = new int[arrN.Length + 1];
                 for (int i = 0; i < newArrN.Length - 1; i++)
-                {
                     newArrN[i] = 1;
-                }
+
                 newArrN[arrN.Length] = lastNum - 1;
                 SplitNum(newArrN);
             }
